c16d44c04b14372285800637891026cfa1d54d67
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-10-11 16:30:25 +0100

    Added quillstack/monolog-factory, added App\Config

--- src/QuillStack/Framework/App.php
+++ src/QuillStack/Framework/App.php
@@ -4,26 +4,12 @@ declare(strict_types=1);
 
 namespace QuillStack\Framework;
 
-use Psr\Http\Message\StreamInterface;
-use Psr\Http\Message\UriFactoryInterface;
 use QuillStack\DI\Container;
+use QuillStack\Framework\App\Config;
 use QuillStack\Framework\App\Kernel;
-use QuillStack\Framework\InstanceFactories\RequestClassFactory;
-use QuillStack\Framework\Interfaces\RequestInterface;
-use QuillStack\Http\Stream\InputStream;
-use QuillStack\Http\Uri\Factory\UriFactory;
 
 final class App
 {
-    /**
-     * @var array
-     */
-    private const DEFAULT_CONFIG = [
-        StreamInterface::class => InputStream::class,
-        UriFactoryInterface::class => UriFactory::class,
-        RequestInterface::class => RequestClassFactory::class,
-    ];
-
     /**
      * @var Container
      */
@@ -35,7 +21,7 @@ final class App
     public function __construct(array $config = [])
     {
         $this->container = new Container(
-            array_merge(self::DEFAULT_CONFIG, $config)
+            array_merge(Config::DEFAULT_CONFIG, $config)
         );
     }
 
---
+++ src/QuillStack/Framework/App/Config.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuillStack\Framework\App;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Log\LoggerInterface;
+use QuillStack\Framework\InstanceFactories\RequestClassFactory;
+use QuillStack\Framework\Interfaces\RequestInterface;
+use QuillStack\Http\Stream\InputStream;
+use QuillStack\Http\Uri\Factory\UriFactory;
+use QuillStack\MonologFactory\FileLoggerClassFactory;
+
+final class Config
+{
+    /**
+     * @var array
+     */
+    public const DEFAULT_CONFIG = [
+        StreamInterface::class => InputStream::class,
+        UriFactoryInterface::class => UriFactory::class,
+        RequestInterface::class => RequestClassFactory::class,
+        LoggerInterface::class => FileLoggerClassFactory::class,
+    ];
+
+}
