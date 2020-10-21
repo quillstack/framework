3c5c0e890adb96ce4acbf707a0b62f46ed530d5c
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-10-21 22:38:16 +0100

    Added AppService class

---
+++ src/QuillStack/Framework/Exceptions/ArgumentTypeNotAllowedException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuillStack\Framework\Exceptions;
+
+use QuillStack\Framework\QuillStackException;
+
+final class ArgumentTypeNotAllowedException extends QuillStackException
+{
+}
---
+++ src/QuillStack/Framework/QuillStackException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuillStack\Framework;
+
+use RuntimeException;
+
+class QuillStackException extends RuntimeException
+{
+}
